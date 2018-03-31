8c21d8a3a6ee03f1f8d89930f4d5d3a70c7741ed
Charles Pierson <charles.pierson15@gmail.com>
2018-03-30 23:39:31 -0400

    Rights and project start date added

--- web.py
+++ web.py
@@ -1,3 +1,11 @@
+'''
+Created by Charles Pierson
+	ALL RIGHTS RESERVED
+
+(Project started on August 21, 2017)
+'''
+
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import smtplib
@@ -14,40 +22,24 @@ soup = BeautifulSoup(page, 'html.parser')
 
 # finds all divs with the class of banner-container and saves it into a variable
 
-# NOTE:
-# DEV: If no more emails, check to see if the cells names have changed
-# 
-###########################################################################################
-# ORIGINAL VARIABLES AND CLASSES
-# col_one = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[0]
-# print(col_one.text)
-
-# col_two = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[1]
-# print(col_two.text)
-
-# col_three = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[2]
-# print(col_three.text)
-
-# col_four = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[3]
-# print(col_four.text)
-##############################################################################################
-
-col_one = soup.find_all('div', attrs={'class' : 'section brand small last grid'})[0]
+'''
+NOTE: FOR ANY ISSUES REGARDING NOT RECIEVING AN EMAIL.
+DEV: If no more emails, check to see if the cells names have changed or if sender email is blocking for security reasons--- from google
+'''
+col_one = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[0]
 print(col_one.text)
 
-col_two = soup.find_all('div', attrs={'class' : 'section brand small last grid'})[1]
+col_two = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[1]
 print(col_two.text)
 
-col_three = soup.find_all('div', attrs={'class' : 'section brand small last grid'})[2]
+col_three = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[2]
 print(col_three.text)
 
-col_four = soup.find_all('div', attrs={'class' : 'section brand small last grid'})[3]
+col_four = soup.find_all('div', attrs={'class' : 'banner-container clearfix two-cells'})[3]
 print(col_four.text)
-
 
 #This removes the text file so it can be re-written
 os.remove("FinalOutput.txt")
-
 
 #opens text file and prints the saved deals to the text document
 with open('FinalOutput.txt', "w") as text_file:
@@ -64,25 +56,22 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
-#############################PUT ADDRESSES HERE ################################
 #gets the from address and the recipiant address
-fromaddr=""
-toaddr=""
-################################################################################
+fromaddr="cpbbwinfo@gmail.com"
+toaddr="charles.pierson15@gmail.com"
+
 msg = MIMEMultipart()
 
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "Bath and Body Top Offers %s" % datetime.now()
+msg['Subject'] = "Bath and Body Works Top Offers %s" % datetime.now()
 body = ("BBW Top Offers")
-
 
 msg.attach(MIMEText(body, 'plain'))
 
+# Sets the file name and sets the location of the file
 filename="FinalOutput.txt"
-######################PUT LOCATION OF YOUR TXT FILE HERE########################
-attachment = open("C:\\Users\\USER NAME\\Desktop\\FinalOutput.txt", "rb")
-################################################################################
+attachment = open("C:\\Users\\CJ' PC\\Desktop\\Python\\wb\\FinalOutput.txt", "rb")
 
 part = MIMEBase('application', 'octet-stream')
 part.set_payload((attachment).read())
@@ -90,12 +79,9 @@ encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
 msg.attach(part)
- 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-###########################FILL IN EMAIL AND PASSWORD#####################
-server.login('EMAIL', "PASSWORD")
-##################################
+server.login('cpbbwinfo@gmail.com', "ibLeGinGOViOUNcaSt")
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
